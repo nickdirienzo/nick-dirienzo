370246a33e77cd82c068bba23b1f37b9d110c82d
Nick DiRienzo <dirienzo.n@gmail.com>
2012-03-29 13:21:56 -0400

    Blog is working with inline admin functions.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 
 import os
 import re
+import markdown2
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
@@ -15,53 +16,127 @@ from google.appengine.api import mail
 from google.appengine.ext import db
 
 class Post(db.Model):
-    path = db.StringProperty()
-    title = db.StringProperty()
-    content = db.TextProperty()
+    slug = db.StringProperty(required=True)
+    title = db.StringProperty(required=True)
+    markdown = db.TextProperty(required=True)
+    html = db.TextProperty(required=True)
+    published = db.BooleanProperty(default=False)
     timestamp = db.DateTimeProperty(auto_now_add=True)
 
 class Page(webapp.RequestHandler):
-    def render(self, page_name, dir, template_vars={}):
-        vars = {}
+    def render(self, page_name, slug="", template_vars={}):
+        page_vars = {}
         path = os.path.join(os.path.dirname(__file__), "templates", page_name + ".html")
+        if not slug:
+            slug = page_name if page_name != "home" else ""
         user = users.get_current_user()
         if user:
-            vars["username"] = user.nickname()
-            vars["logout_url"] = users.create_logout_url(dir)
-            vars["is_admin"] = users.is_current_user_admin()
+            page_vars["username"] = user.nickname()
+            page_vars["logout_url"] = users.create_logout_url(slug)
+            page_vars["is_admin"] = users.is_current_user_admin()
         else:
-            vars["username"] = None
-            vars["login_url"] = users.create_login_url(dir)
-        vars["page_name"] = page_name
-        vars.update(template_vars)
-        self.response.out.write(template.render(path, vars))
+            page_vars["username"] = None
+            page_vars["login_url"] = users.create_login_url(slug)
+        page_vars["page_name"] = page_name
+        page_vars.update(template_vars)
+        self.response.out.write(template.render(path, page_vars))
 
-class StaticPageHandler(Page):
+class PageHandler(Page):
     def get(self, page_name):
         if page_name == "":
-            self.render("home", "/")
+            self.render("home")
         elif page_name == "projects":
-            self.render("projects", "/projects")
+            self.render("projects")
         elif page_name == "talks":
-            self.render("talks", "/talks")
+            self.render("talks")
+        elif page_name == "posts":
+            self.redirect("/posts/")
         else:
-            self.render("404", "/uh-oh")
+            self.render("oops")
 
 class PostHandler(Page):
-    def get(self, path):
-        if path:
-            post = BlogPost.all().filter("path = ", path).get()
+    def get(self, slug):
+        if slug:
+            post = Post.all().filter("slug = ", slug).get()
             if post:
-                self.render("blog_post", "/post/%s" % path, {"post": post})
+                self.render("post", "/post/%s" % slug, {"post": post})
             else:
-                self.redirect("/404")
+                self.redirect("/oops")
         else:
             self.render("posts", "/posts", {"posts": Post.all().order("-timestamp")})
 
+class PostPublishHandler(webapp.RequestHandler):
+    def post(self, slug):
+        if users.is_current_user_admin():
+            post = Post.all().filter("slug = ", slug).get()
+            post.published = True
+            post.put()
+            self.redirect("/posts/")
+        else:
+            self.redirect("/")
+
+class PostUnpublishHandler(webapp.RequestHandler):
+    def post(self, slug):
+        if users.is_current_user_admin():
+            post = Post.all().filter("slug = ", slug).get()
+            post.published = False
+            post.put()
+            self.redirect("/posts/")
+        else:
+            self.redirect("/")
+
+class PostDeleteHandler(webapp.RequestHandler):
+    def post(self, slug):
+        if users.is_current_user_admin():
+            db.delete(Post.all().filter("slug = ", slug).get())
+            self.redirect("/posts/")
+        else:
+            self.redirect("/")
+
+class PostEditorHandler(Page):
+    def get(self, slug=""):
+        if slug:
+            post = Post.all().filter("slug = ", slug).get()
+            if post:
+                self.render("post_editor", "/posts/edit/%s" % slug, {"title": post.title, "markdown": post.markdown})
+            else:
+                self.render("post_editor")
+        else:
+            self.render("post_editor")
+
+    def post(self, slug=""):
+        def slugify(title):
+            """ Not sure where I found this RegEx, but I am still working on learning it myself. """
+            title = title.lower()
+            return re.sub(re.compile(r"[^a-zA-Z0-9-]+"), "", re.sub(re.compile("\s"), "-", title))
+        
+        if users.is_current_user_admin():
+            title = self.request.get("pt")
+            markdown = self.request.get("c")
+            html = markdown2.markdown(markdown)
+            if slug:
+                post = Post.all().filter("slug = ", slug).get()
+                post.title = title
+                post.markdown = markdown
+                post.html = html
+                post.slug = slugify(title)
+                post.put()
+            else:
+                slug = slugify(title)
+                post = Post(slug=slug, title=title, markdown=markdown, html=html)
+                post.put()
+            self.redirect("/posts/%s" % slug)
+        else:
+            self.redirect("/")
+
 def main():
     application = webapp.WSGIApplication(
-                                        [("/posts/(.*)", PostHandler),
-                                        ("/(.*)", StaticPageHandler)],
+                                        [("/posts/delete/(.*)", PostDeleteHandler),
+                                        ("/posts/unpublish/(.*)", PostUnpublishHandler),
+                                        ("/posts/publish/(.*)", PostPublishHandler),
+                                        ("/posts/edit/(.*)", PostEditorHandler),
+                                        ("/posts/(.*)", PostHandler),
+                                        ("/(.*)", PageHandler)],
                                          debug=True)
     util.run_wsgi_app(application)
 
